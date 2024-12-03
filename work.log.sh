ed32c0a5c7db6c34925378535f9817db52cbae4e
Shujia Huang <huangshujia9@gmail.com>
2024-12-03 20:05:13 +0800

    delete un-using command

--- work.log.sh
+++
@@ -1,7 +0,0 @@
-
-rsync -avtp ../rohitsalecha.com/themes/academic ./theme/
-rsync -avtp ../rohitsalecha.com/config.toml ./
-rsync -avtp ../rohitsalecha.com/config ./
-rsync -avtp ../rohitsalecha.com/static ./
-rsync -avtp ../rohitsalecha.com/content ./
-rsync -avtp ../rohitsalecha.com/layouts ./
